7520c39d78c837b3d270dd7e82772765611721ef
To Nhat Bao <tonhatbao@gmail.com>
2024-12-24 03:49:59 +0700

    init

---
+++ cnpm/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from urllib.parse import quote
+from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+import cloudinary
+
+app = Flask(__name__)
+app.secret_key = 'KJHJF^(&*&&*OHH&*%&*TYUGJHG&(T&IUHKB'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/phongmachdb?charset=utf8mb4" % quote('admin123')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["PAGE_SIZE"] = 8
+
+db = SQLAlchemy(app=app)
+# login = LoginManager(app=app)
