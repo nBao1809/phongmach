3aff35c51b59d828b8077e06a8885cb656fde179
nBao <tonhatbao@gmail.com>
2024-12-26 01:17:08 +0700

    nothing

--- cnpm/app/index.py
+++ cnpm/app/index.py
@@ -1,5 +1,5 @@
-from flask import render_template,request,redirect,url_for,flash
-from flask_login import login_user, logout_user, login_required
+from flask import render_template,request,redirect
+from flask_login import login_user, logout_user
 from app import app,dao,login
 import admin
 
