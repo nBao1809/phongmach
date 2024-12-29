d141cdcf52545182ccb9d940a5f5c115fe847106
nBao <tonhatbao@gmail.com>
2024-12-29 22:12:50 +0700

    done datlich

--- cnpm/app/dao.py
+++ cnpm/app/dao.py
@@ -23,11 +23,11 @@ def get_user_by_id(id):
 def get_patient_by_phone(phone):
     return Patient.query.filter(Patient.sdt.__eq__(phone)).first()
 
+
 # def add_shedule(date):
 #     limit=DailyPatientLimit.query.first()
 
 def add_appointment_list(date):
-
     # Kiểm tra xem cuộc hẹn đã tồn tại cho ngày này chưa
     existing_appointment = Appointment_list.query.filter(Appointment_list.date == date).first()
     if existing_appointment:
@@ -43,6 +43,8 @@ def add_appointment_list(date):
     return new_appointment  # Trả về đối tượng cuộc hẹn mới
 
 
+def get_patient_appointment(patient_id, appointment_id):
+    return Patient_Appointment.query.filter(Patient_Appointment.appointment_id == appointment_id).first()
 
 
 def make_appointment(patient_id, appointment):
@@ -59,7 +61,10 @@ def make_appointment(patient_id, appointment):
     # Thêm đối tượng vào cơ sở dữ liệu
     db.session.add(new_patient_appointment)
     db.session.commit()  # Lưu thay đổi vào cơ sở dữ liệu
+    # lấy patient_appointment vừa thêm vào
+    patient_appointment = get_patient_appointment(patient_id=patient.id, appointment_id=appointment.id)
     return {
+        'patient_appointment_id': patient_appointment.id,
         'patient_id': patient.id,
         'appointment_id': appointment.id,
         'status': appointment.status,
